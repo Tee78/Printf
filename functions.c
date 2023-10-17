f6c8e911a8a7e3cd2e7514f08b7af7b48eb681a6
Tee78 <ajokeadeoluwa971@gmail.com>
2023-10-17 14:21:10 -0700

    functions.c

--- functions.c
+++ functions.c
@@ -23,7 +23,7 @@ int print_char(va_list types, char buffer[],
 /**
  * print_string - Prints a string
  * @types: List a of arguments
- * @buffer: Buffer array to handle print
+ * @buffer: This buffers array to handle print
  * @flags:  Calculates active flags
  * @width: get width.
  * @precision: Precision specification
